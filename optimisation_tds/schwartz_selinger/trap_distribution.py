e8ad8d7e32fbe4658319e5b7482d500749edc563
J Dark <james.dark.95@gmail.com>
2022-09-02 13:19:28 +0000

    plotting script for trap dist

---
+++ optimisation_tds/schwartz_selinger/trap_distribution.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.linspace(0, 10e-06, num=1000)
+line = 1e25 / (1 + np.exp((x - 3e-06) / 5e-07)) + 1e24
+
+plt.figure()
+plt.vlines(3e-06, 0, 1e26, linestyles="dashed", color="black")
+plt.vlines(7e-06, 0, 1e26, color="black")
+plt.plot(x, line)
+# plt.yscale("log")
+plt.ylim(7.5e23, 1.2e25)
+plt.xlim(0, 10e-06)
+
+plt.show()
