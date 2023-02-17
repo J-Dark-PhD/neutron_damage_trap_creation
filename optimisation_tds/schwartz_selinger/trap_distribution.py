1583434fdd7d8e62e0240bd527ae60a15637bbb8
J Dark <james.dark.95@gmail.com>
2023-02-17 13:23:11 +0000

    more generic

--- optimisation_tds/schwartz_selinger/trap_distribution.py
+++ optimisation_tds/schwartz_selinger/trap_distribution.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x = np.linspace(0, 15e-06, num=1000)
+x = np.linspace(0, 5e-06, num=1000)
 line = 1 / (1 + np.exp((x - 3e-06) / 5e-07))
 
 plt.rc("text", usetex=True)
@@ -12,10 +12,11 @@ plt.figure()
 # plt.vlines(3e-06, 0, 1e26, linestyles="dashed", color="black")
 # plt.vlines(7e-06, 0, 1e26, color="black")
 plt.plot(x, line, color="black")
-plt.yscale("log")
-plt.ylabel(r"Trap density (m$^{-3}$)")
+# plt.yscale("log")
+plt.ylabel(r"$\frac{n_{t}(x)}{n_{t}}$", fontsize=18)
 plt.xlabel(r"x (m)")
-plt.ylim(5e-04, 5e0)
+plt.ylim(0, 1)
+# plt.yticks([])
 plt.xlim(0, 5e-06)
 ax = plt.gca()
 ax.spines["top"].set_visible(False)
