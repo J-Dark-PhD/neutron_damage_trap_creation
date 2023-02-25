b7bccd8c8f55f9df8c963327b63f9e2b0a2cdb36
J Dark <james.dark.95@gmail.com>
2023-02-25 14:10:39 +0000

    testing T values, print statement for case

--- parametric_study_transient_testing.py
+++ parametric_study_transient_testing.py
@@ -2,12 +2,18 @@ from neutron_induced_traps_model import festim_sim
 import numpy as np
 
 
-dpa_values = np.geomspace(1e-03, 1e03, num=7)
+dpa_values = np.geomspace(1e-03, 1e03, num=50)
+temperature_values = np.linspace(1300, 400, num=50)[:4]
 
-for dpa in dpa_values:
-    festim_sim(
-        T=761,
-        dpa=dpa,
-        results_folder_name="Results/transient_testing/{:.0e}_dpa/".format(dpa),
-        transient_run=True
-    )
+for temperature in temperature_values:
+    for dpa in dpa_values:
+        print("Running case: dpa={:.2e}, T={:.0f}".format(dpa, temperature))
+        festim_sim(
+            T=temperature,
+            dpa=dpa,
+            results_folder_name="Results/parametric_studies/case_1e09s/dpa={:.2e}/T={:.0f}/".format(
+                dpa, temperature
+            ),
+            transient_run=True,
+            total_time=1e09,
+        )
