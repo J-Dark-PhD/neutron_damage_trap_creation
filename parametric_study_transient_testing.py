5007f43eb485b2edddf74b7fb23328b69b3fd480
J Dark <james.dark.95@gmail.com>
2023-03-10 09:36:11 +0000

    additional cases and check if file exists first

--- parametric_study_transient_testing.py
+++ parametric_study_transient_testing.py
@@ -1,5 +1,6 @@
 from neutron_induced_traps_model import festim_sim
 import numpy as np
+from os.path import exists
 
 
 dpa_values = np.geomspace(1e-03, 1e03, num=50)
@@ -53,10 +54,9 @@ def case_1fpy():
     #         )
     test_temperaure_values = np.linspace(400, 900, num=6)
     test_dpa_values = np.geomspace(1e-05, 1e01, num=7)
-    # test_dpa_values = [1e-01]
 
     for temperature in test_temperaure_values:
-        for dpa in test_dpa_values[4:]:
+        for dpa in test_dpa_values:
             print("Running case: dpa={:.2e}, T={:.0f}".format(dpa, temperature))
             festim_sim(
                 T=temperature,
@@ -69,6 +69,51 @@ def case_1fpy():
             )
 
 
+def case_1e05s():
+    dpa_values = np.geomspace(1e-03, 1e03, num=10)
+
+    for dpa in dpa_values:
+        print("Running case: dpa={:.2e}".format(dpa))
+        festim_sim(
+            T=700,
+            dpa=dpa,
+            results_folder_name="Results/parametric_studies/case_1e05s/T=700K/dpa={:.2e}/".format(
+                dpa
+            ),
+            transient_run=True,
+            total_time=1e05,
+        )
+
+
+def case_24h():
+    dpa_values = np.geomspace(1e-05, 1e03, num=100)
+    temperature_values = np.linspace(400, 1300, num=100)
+
+    results_folder = "Results/parametric_studies/case_24/"
+
+    for temperature in temperature_values:
+        for dpa in dpa_values:
+
+            filename_test = (
+                results_folder
+                + "dpa={:.1e}/T={:.0f}/derived_quantities.csv".format(dpa, temperature)
+            )
+            if exists(filename_test):
+                continue
+            else:
+                print("Running case: dpa={:.2e}, T={:.0f}".format(dpa, temperature))
+                festim_sim(
+                    T=temperature,
+                    dpa=dpa,
+                    results_folder_name=results_folder
+                    + "dpa={:.2e}/T={:.0f}/".format(dpa, temperature),
+                    transient_run=True,
+                    total_time=24 * 3600,
+                )
+
+
 # case_steady()
 # case_1e09s()
-case_1fpy()
+# case_1fpy()
+# case_1e05s()
+case_24h()
