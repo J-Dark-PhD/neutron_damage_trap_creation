6dae5c33718bd4422768acc1c4937932a3b42990
James laptop <james.dark.95@gmail.com>
2023-04-26 15:56:13 +0200

    file moved

--- Results/plotting_scripts/plot_damage_distribution.py
+++ Results/plotting_scripts/plot_damage_distribution.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 x = np.linspace(0, 1e-5, num=1000)
 
 traps = 1 / (1 + np.exp((x - 2.5e-06) / 5e-07))
-
 
 plt.rc("text", usetex=True)
 plt.rc("font", family="serif", size=12)
@@ -14,8 +12,8 @@ plt.figure()
 plt.plot(x, traps, color="black")
 plt.xlim(0, 1e-05)
 plt.ylim(0, 1)
-plt.ylabel("Trap density ratio")
-plt.xlabel("x (m)")
+plt.ylabel(r"Trap density ratio")
+plt.xlabel(r"x (m)")
 ax = plt.gca()
 ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
